b89008c58cb1300325c9209b720aac3f15e664b7
Daisy Jansen <daisy.jansen@student.pxl.be>
2020-11-16 21:39:49 +0100

    Added calculateStrength method in PasswordUtil with associated Unit tests

--- src/main/java/be/pxl/ja/streamingservice/util/PasswordUtil.java
+++ src/main/java/be/pxl/ja/streamingservice/util/PasswordUtil.java
@@ -5,6 +5,54 @@ public class PasswordUtil {
 	private static final String SPECIAL_CHARACTERS = "~!@#$%^&*()_-";
 
 	public static int calculateStrength(String password) {
-		return 5;
+
+		int score = 0;
+
+		if (password.length() < 6) {
+			score = 0;
+		}
+		if (password.length() >= 6) {
+			if (password.length() <= 10) {
+				score = 1;
+			}
+			if (password.length() > 10) {
+				score = 2;
+			}
+
+			boolean containsNumber = false;
+			boolean containsLowercase = false;
+			boolean containsUppercase = false;
+			boolean containsSpecialCharacter = false;
+
+			for (int i = 0; i < password.length(); i++) {
+				if (Character.isDigit(password.charAt(i))) {
+					containsNumber = true;
+				}
+				if (Character.isLowerCase(password.charAt(i))) {
+					containsLowercase = true;
+				}
+				if (Character.isUpperCase(password.charAt(i))) {
+					containsUppercase = true;
+				}
+				if (SPECIAL_CHARACTERS.indexOf(password.charAt(i)) != -1) {
+					containsSpecialCharacter = true;
+				}
+			}
+
+			if (containsNumber) {
+				score += 2;
+			}
+			if (containsLowercase) {
+				score += 2;
+			}
+			if (containsUppercase) {
+				score += 2;
+			}
+			if (containsSpecialCharacter) {
+				score += 2;
+			}
+		}
+
+		return score;
 	}
 }
--- src/test/java/be/pxl/ja/streamingservice/util/PasswordUtilCalculateStrengthTest.java
+++ src/test/java/be/pxl/ja/streamingservice/util/PasswordUtilCalculateStrengthTest.java
@@ -1,5 +1,49 @@
 package be.pxl.ja.streamingservice.util;
 
+import be.pxl.ja.streamingservice.model.Account;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PasswordUtilCalculateStrengthTest {
 
+    @Test
+    public void PasswordStrengthWithLessThanSixCharactersReturnsZero() {
+        assertEquals(0, PasswordUtil.calculateStrength("pxl"));
+    }
+
+    @Test
+    public void PasswordStrengthWithMoreThanSixButLessThanTenCharactersWithOnlyLowercaseCharactersReturnsThree() {
+        assertEquals(3, PasswordUtil.calculateStrength("pxlpxlpxl"));
+    }
+
+    @Test
+    public void PasswordStrengthWithMoreThanSixButLessThanTenCharactersWithOnlyUppercaseCharactersReturnsThree() {
+        assertEquals(3, PasswordUtil.calculateStrength("PXLPXL"));
+    }
+
+    @Test
+    public void PasswordStrengthWithMoreThanSixButLessThanTenCharactersWithOnlyNumericalCharactersReturnsThree() {
+        assertEquals(3, PasswordUtil.calculateStrength("1234567"));
+    }
+
+    @Test
+    public void PasswordStrengthWithMoreThanSixButLessThanTenCharactersWithOnlySpecialCharactersReturnsThree() {
+        assertEquals(3, PasswordUtil.calculateStrength("~!@#$^&*("));
+    }
+
+    @Test
+    public void PasswordStrengthWithMoreThanSixButLessThanTenCharactersWithLowercaseAndUppercaseCharactersReturnsFive() {
+        assertEquals(5, PasswordUtil.calculateStrength("PxlPxl"));
+    }
+
+    @Test
+    public void PasswordStrengthWithMoreThanTenCharactersWithLowercaseAndUppercaseCharactersReturnsSix() {
+        assertEquals(6, PasswordUtil.calculateStrength("PxlPxlPxlPxl"));
+    }
+
+    @Test
+    public void PasswordStrengthWithMoreThanTenCharactersWithLowercaseAndUppercaseAndNumericalAndSpecialCharactersReturnsTen() {
+        assertEquals(10, PasswordUtil.calculateStrength("Pxl123~pxlpxl"));
+    }
 }
